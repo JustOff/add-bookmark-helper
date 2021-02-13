7f12655c8fd8d944275154148bec6457115b4e57
JustOff <Off.Just.Off@gmail.com>
2021-02-13 20:49:41 +0200

    Set up release automation

---
+++ release.cmd
@@ -0,0 +1,9 @@
+@echo off
+set VER=1.0.6
+
+sed -i -E "s/version>.+?</version>%VER%</" install.rdf
+sed -i -E "s/version>.+?</version>%VER%</; s/download\/.+?\/add-bookmark-helper-.+?\.xpi/download\/%VER%\/add-bookmark-helper-%VER%\.xpi/" update.xml
+
+set XPI=add-bookmark-helper-%VER%.xpi
+if exist %XPI% del %XPI%
+zip -r9q %XPI% * -x .git/* .gitignore update.xml LICENSE README.md *.cmd *.xpi *.exe
